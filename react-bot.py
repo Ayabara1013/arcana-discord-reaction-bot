fd0fdec95ee03aabc105a6524eacb71280489863
Jeremy Allan <jallan8210@gmail.com>
2024-06-08 00:50:59 -0400

    fixed commit nightmare

--- react-bot.py
+++ react-bot.py
@@ -1,8 +1,43 @@
 # import the necessary libraries from discord.py
 import discord
 from discord.ext import commands
-
 import emoji
+import boto3
+from botocore.exceptions import ClientError
+
+
+# stuff for the secret? amazon gave it to me
+def get_secret():
+
+    secret_name = "arcana-reaction-bot-token"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return secret
+
+
+
+# Get the bot token from AWS Secrets Manager
+bot_token = get_secret()
+
+
+
 
 # define the bot's intentions, specifying it should recieve message content
 intents = discord.Intents.default()
@@ -104,3 +139,7 @@ async def react_set(ctx):
 # helper function to extract emojis
 def extract_emojis(message_content):
   return ''.join(c for c in message_content if c in emoji.UNICODE_EMOJI)
+
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot.run(bot_token)
